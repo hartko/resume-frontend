f98853da2de4849735961c58554c8d2caf8e7421
Harriette Mae Rodriguez <haryetmey.rodriguez@gmail.com>
2024-01-19 21:16:46 +0800

    image

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -4,14 +4,15 @@
       <b-col cols="12" class="mb-4 mt-5">
         <b-img
           style="image-rendering: pixelated"
-          :src="require('../../assets/hart123.jpg')"
+          :src="require('../../assets/Hart.jpg')"
           v-bind="mainProps"
           rounded="circle"
           alt="Circle image"
         ></b-img
       ></b-col>
-      <b-col cols="12" class="mb-2"><h5>HARRIETTE RODRIGUEZ</h5></b-col>
+      <b-col cols="12" class="mb-2"><h3>HARRIETTE RODRIGUEZ</h3></b-col>
       <b-col cols="12"><h5>WEB DEVELOPER</h5></b-col>
+      <b-col cols="12"><h5>PROFESSIONAL MANGINGINOM🍻</h5></b-col>
     </b-row>
   </b-container>
 </template>
